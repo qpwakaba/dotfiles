2544199cc5e74fb19e197eb233a28432c82133a2
King (qpwakaba) <King.qpwakaba@gmail.com>
2019-12-26 23:43:54 +0900

    Add check $HOME/.local/profile exists

--- install.sh
+++ install.sh
@@ -13,7 +13,9 @@ done
 if ! [ -e "$HOME/.local" ]; then
   mkdir "$HOME/.local"
 fi
-sed -ie '/^export DOTFILES_HOME=.*$/d' "$HOME/.local/profile"
+if [ -f "$HOME/.local/profile" ]; then
+  sed -ie '/^export DOTFILES_HOME=.*$/d' "$HOME/.local/profile"
+fi
 echo "export DOTFILES_HOME=$(cd "$(dirname "$0")" && echo "$(pwd)")" >>"$HOME/.local/profile"
 
 
